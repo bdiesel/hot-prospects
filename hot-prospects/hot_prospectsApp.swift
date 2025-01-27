e4e7467303cb5d92ca8a3e40c489fdefdfd76769
Brian Diesel <bri@ndiesel.net>
2025-01-27 12:49:04 -0500

    Initial Commit

---
+++ hot-prospects/hot_prospectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hot_prospectsApp.swift
+//  hot-prospects
+//
+//  Created by Brian Diesel on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct hot_prospectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
