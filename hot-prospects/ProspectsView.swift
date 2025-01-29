92887f7e5e44527ae396f3689846b0c80e5c267e
Brian Diesel <bri@ndiesel.net>
2025-01-29 10:58:06 -0500

    Add QR scanning feature

--- hot-prospects/ProspectsView.swift
+++ hot-prospects/ProspectsView.swift
@@ -5,6 +5,7 @@
 //  Created by Brian Diesel on 1/29/25.
 //
 
+import CodeScanner
 import SwiftData
 import SwiftUI
 
@@ -15,7 +16,8 @@ struct ProspectsView: View {
 
     @Environment(\.modelContext) var modelContext
     @Query(sort: \Prospect.name) var prospects: [Prospect]
-
+    @State private var isShowingScanner = false
+    
     let filter: FilterType
 
     var title: String {
@@ -54,16 +56,34 @@ struct ProspectsView: View {
             .navigationTitle(title)
             .toolbar {
                 Button("Scan", systemImage: "qrcode.viewfinder") {
-                                let prospect = Prospect(
-                                    name: "Brian Diesel",
-                                    emailAddress: "brian@designcondition.com",
-                                    isContacted: false)
-                        modelContext.insert(prospect)
-                    }
+                    isShowingScanner = true
+                    //modelContext.insert(prospect)
+                }
+            }
+            .sheet(isPresented: $isShowingScanner){
+                CodeScannerView(
+                    codeTypes: [.qr],
+                    simulatedData: "Brian Diesel\nbrian@designcondition.com",
+                    completion: handleScanResult
+                )
             }
         }
     }
     
+    func handleScanResult(result: Result<ScanResult, ScanError>) {
+       isShowingScanner = false
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+
+            let person = Prospect(name: details[0], emailAddress: details[1], isContacted: false)
+
+            modelContext.insert(person)
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
+        }
+    }
 }
 
 #Preview {
