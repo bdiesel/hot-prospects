021e956452db7260e20ce90f5284f7fee04897b9
Brian Diesel <bri@ndiesel.net>
2025-01-30 08:51:05 -0500

    make qr code editable and sharable

--- hot-prospects/MeView.swift
+++ hot-prospects/MeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct MeView: View {
     @AppStorage("name") private var name = "Anonymous"
     @AppStorage("emailAddress") private var emailAddress = "you@yoursite.com"
+    @State private var qrCode = UIImage()
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
@@ -25,14 +26,24 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu{
+                        ShareLink(item: Image(uiImage: qrCode), preview: SharePreview("My QR Code", image: Image(uiImage: qrCode)))
+                    }
             }
             .navigationTitle("Your code")
+            .onAppear(perform: updateCode)
+            .onChange(of: emailAddress,updateCode)
+            .onChange(of: name, updateCode)
         }
+    }
+    
+    func updateCode(){
+        qrCode = generateQRCode(from: "\(emailAddress),\(name)")
     }
     
     func generateQRCode(from string: String) -> UIImage {
@@ -40,7 +51,8 @@ struct MeView: View {
 
         if let outputImage = filter.outputImage {
             if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
-                return UIImage(cgImage: cgImage)
+                qrCode = UIImage(cgImage: cgImage)
+                return qrCode
             }
         }
 
